e0e2b7b6f5b6966e3ea966d2b703b0b076b351c5
JoaoVictorFBarros <joaovfrancisco2001@gmail.com>
2023-12-01 10:09:57 -0300

    commit

---
+++ Front/app.js
@@ -0,0 +1,288 @@
+function submitForm(data,path) {
+  const apiUrl = 'http://localhost:3000/api/' + path;
+
+  axios.post(apiUrl, data)
+      .then(response => {
+          alert('Cadastro concluído com sucesso!');
+          clearForm();
+      })
+      .catch(error => {
+          alert('Erro ao cadastrar: ' + error.response.data.error);
+      });
+}
+
+let salvaStudents = []
+function addStudent() {
+  const name = document.getElementById('name').value;
+  const rawDate = document.getElementById('date').value;
+  const gradeReference = document.getElementById('gradeReference').value.split(',');
+
+  if (!rawDate || isNaN(new Date(rawDate))) {
+    alert('Data inválida!');
+    return;
+  }
+
+  const formattedDate = new Date(rawDate).toISOString();
+  const studentData = {
+      name,
+      date: [formattedDate],
+      gradeReference: gradeReference.map(reference => Number(reference.trim()))
+  }; 
+
+  salvaStudents.push(studentData);
+  
+}
+
+function addToGrade(){
+  const description = document.getElementById("description").value
+  const regex = /^(seg|ter|qua|qui|sex)\s-\s\d{2}:\d{2}\s-\s\d{2}:\d{2}$/;
+
+  if (regex.test(description)){
+    const grade = [{
+      "_id": Number(document.getElementById("nextGrade").value),
+      "description": description
+    }]
+    submitForm({"Grade":grade},'grades')
+    
+  }
+  else{
+    alert("A entrada está em um formato inválido. Siga o exemplo: seg - 07:00 - 08:00")
+  }
+}
+
+function addMonitor() {
+  const name = document.getElementById('nameMonitor').value;
+  const gradeReference = document.getElementsByClassName('checkbox')
+  let selectedReferences = []
+
+  Array.from(gradeReference).forEach(checkbox => {
+    if(checkbox.checked){
+      selectedReferences.push(checkbox.value)
+    }
+  })
+
+  const monitorData = {
+    name,
+    gradeReference: selectedReferences
+  }
+
+  submitForm(monitorData,'monitors')
+}
+
+function showPopup(studentData, index) {
+  const popupContent = document.getElementById('popup-content');
+  popupContent.innerHTML = `
+      <strong>Nome:</strong> ${studentData.name} - 
+      <strong>Data:</strong> ${studentData.date} - 
+      <strong>Referências de Grade:</strong> ${studentData.gradeReference.join(', ')}
+      <p class="warning-message">Após cadastrar, não será mais possível editar ou excluir.</p>
+      <button onclick="removeStudent(${index})">Remover </button>
+      <button onclick="cadastrarStudent()">Cadastrar</button>
+      <button type="button" onclick="closePopup()">Fechar</button>
+  `;
+
+  const popup = document.getElementById('popup');
+  popup.style.display = 'block';
+}
+
+function cadastrarStudent() {
+  const latestStudentIndex = salvaStudents.length - 1;
+  const latestStudentData = salvaStudents[latestStudentIndex];
+  submitForm(latestStudentData,'students');
+  closePopup(); 
+}
+
+function removeStudent(index) {
+  salvaStudents.splice(index, 1);
+  alert('Estudante removido com sucesso!');
+  clearForm();
+  console.log(salvaStudents);
+}
+
+function closePopup() {
+  const popup = document.getElementById('popup');
+  popup.style.display = 'none';
+}
+
+function clearForm() {
+    document.getElementById('name').value = '';
+    document.getElementById('date').value = '';
+    document.getElementById('gradeReference').value = '';
+}
+
+function getCurrentDate() {
+  const today = new Date();
+  let month = today.getMonth() + 1;
+  let day = today.getDate();
+
+  month = month < 10 ? '0' + month : month;
+  day = day < 10 ? '0' + day : day;
+
+  const currentDate = today.getFullYear() + '-' + month + '-' + day;
+  return currentDate;
+}
+
+function getNextDayOfWeek(dayOfWeek,id) {
+  const today = new Date();
+  const currentDayOfWeek = today.getDay();
+  const daysUntilNextDay = (dayOfWeek + 7 - currentDayOfWeek) % 7;
+
+  const nextDay = new Date(today);
+  nextDay.setDate(today.getDate() + daysUntilNextDay);
+
+  document.getElementById(id).innerText = "-  " + nextDay.getDate() + " / " + (nextDay.getMonth() + 1)
+}
+
+function switchToMonitors(){
+  document.getElementById("pageHeader").innerText = "Cadastro de Monitores"
+  document.getElementById("studentForm").style.display = "none"
+  document.getElementById("monitorForm").style.display = "block"
+  document.getElementById("students-display").style.display = "none"
+  document.getElementById("monitors-display").style.display = "block"
+  document.getElementById("gradeForm").style.display = "none"
+  document.getElementById("gradeDisplay").style.display = "none"
+
+  document.getElementById("switchBack").onclick = switchToStudents
+  document.getElementById("switchFoward").onclick = switchToGrade
+}
+
+function switchToStudents(){
+  document.getElementById("pageHeader").innerText = "Cadastro de Estudantes"
+  document.getElementById("studentForm").style.display = "block"
+  document.getElementById("monitorForm").style.display = "none"
+  document.getElementById("students-display").style.display = "block"
+  document.getElementById("monitors-display").style.display = "none"
+  document.getElementById("gradeForm").style.display = "none"
+  document.getElementById("gradeDisplay").style.display = "none"
+
+  document.getElementById("switchBack").onclick = switchToGrade
+  document.getElementById("switchFoward").onclick = switchToMonitors
+}
+
+function switchToGrade(){
+  document.getElementById("pageHeader").innerText = "Cadastro de Grade"
+  document.getElementById("studentForm").style.display = "none"
+  document.getElementById("monitorForm").style.display = "none"
+  document.getElementById("students-display").style.display = "none"
+  document.getElementById("monitors-display").style.display = "none"
+  document.getElementById("gradeForm").style.display = "block"
+  document.getElementById("gradeDisplay").style.display = "block"
+
+  document.getElementById("switchBack").onclick = switchToMonitors
+  document.getElementById("switchFoward").onclick = switchToStudents
+}
+
+window.onload = ()=>{
+
+  var daysOfWeek = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta'];
+
+  const currentDayIndex = new Date().getDay() -1
+  const adjustedIndex = currentDayIndex < 0 ? 4 : currentDayIndex
+  daysOfWeek = [
+    ...daysOfWeek.slice(adjustedIndex),
+    ...daysOfWeek.slice(0, adjustedIndex)
+  ];
+
+  const dict = {
+    'Segunda': 'monday',
+    'Terça': 'tuesday',
+    'Quarta': 'wednesday',
+    'Quinta': 'thursday',
+    'Sexta': 'friday'
+  }
+  
+
+  daysOfWeek.forEach(day => {
+
+    const dayDiv = document.createElement('div');
+    dayDiv.classList.add('day');
+
+    const headerDiv = document.createElement('div');
+    headerDiv.classList.add('day-header');
+
+    const dayParagraph = document.createElement('p');
+    dayParagraph.textContent = day;
+    headerDiv.appendChild(dayParagraph);
+
+    const headerDateDiv = document.createElement('div');
+    headerDateDiv.id = `${dict[day]}`;
+
+    const StudentsListDiv = document.createElement('div');
+    StudentsListDiv.id = `${dict[day]}-list`;
+    StudentsListDiv.classList.add('students-list')
+    
+    dayDiv.appendChild(headerDiv);
+    headerDiv.appendChild(headerDateDiv);
+    dayDiv.appendChild(StudentsListDiv)
+
+    document.getElementById("week").appendChild(dayDiv);
+  });
+
+  getNextDayOfWeek(1,"monday");
+  getNextDayOfWeek(2, 'tuesday');
+  getNextDayOfWeek(3, 'wednesday');
+  getNextDayOfWeek(4, 'thursday');
+  getNextDayOfWeek(5, 'friday');
+}
+
+window.addEventListener('keydown', function(event) {
+  if (event.key === 'Shift') {
+    Array.from(this.document.getElementsByClassName("removeButton")).forEach(box => {
+    box.style.display = "block"
+    })
+  }
+});
+
+window.addEventListener('keyup', function(event) {
+  if (event.key === 'Shift') {
+      Array.from(this.document.getElementsByClassName("removeButton")).forEach(box => {
+        box.style.display = "none"
+      })
+
+  }
+});
+
+function removeStudent(id){
+
+  const apiUrl = 'http://localhost:3000/api/studentsRemove';
+  const data = {"_id":id}
+
+  axios.post(apiUrl, data)
+      .then(response => {
+          alert('Aluno removido com sucesso!');
+          window.location.reload()
+      })
+      .catch(error => {
+          alert('Erro ao remover: ' + error.response.data.error);
+      });
+}
+
+function removeMonitor(id){
+
+  const apiUrl = 'http://localhost:3000/api/monitorsRemove';
+  const data = {"_id":id}
+
+  axios.post(apiUrl, data)
+      .then(response => {
+          alert(response.data.message);
+          window.location.reload()
+      })
+      .catch(error => {
+          alert('Erro ao remover: ' + error.response.data.error);
+      });
+}
+
+function removeGrade(id){
+
+  const apiUrl = 'http://localhost:3000/api/gradesRemove';
+  const data = {"_id":id}
+
+  axios.post(apiUrl, data)
+      .then(response => {
+          alert(response.data.message);
+          window.location.reload()
+      })
+      .catch(error => {
+          alert('Erro ao remover: ' + error.response.data.error);
+      });
+}
---
+++ Front/listagens.js
@@ -0,0 +1,177 @@
+function load_monitors(grade) {
+  fetch("http://localhost:3000/api/monitors")
+    .then((response) => response.json())
+    .then((data) => {
+      const monitorListDiv = document.getElementById("monitorList");
+
+      monitorListDiv.innerHTML = "";
+
+      data.forEach((monitor) => {
+
+        var studentGradeDescription =""
+        monitor.gradeReference.forEach(gradeReference =>{
+          const studentGradeId = gradeReference;
+          const foundGrade = grade.find(
+            (gradeItem) => gradeItem._id === studentGradeId
+          );
+          if (foundGrade) {
+            studentGradeDescription += foundGrade.description;
+            studentGradeDescription += "<br>";
+          } else {
+            console.warn(`Grade com _id ${studentGradeId} não encontrado.`);
+          }
+
+          studentGradeDescription += "<br>"
+        })
+        const monitorItem = document.createElement("div");
+        monitorItem.classList.add("monitorInstance");
+        monitorItem.innerHTML = `<div class="removeButton" style="display: none;"onclick="removeMonitor('${monitor._id}')">X</div><strong>${
+          monitor.name
+        }</strong><br>${studentGradeDescription}`;
+        monitorListDiv.appendChild(monitorItem);
+      });
+    })
+    .catch((error) => {
+      console.error("Erro ao obter os monitores disponíveis:", error);
+    });
+}
+
+function sortGrade(str1, str2) {
+  const extrairDados = (str) => {
+    const match = str.match(/^(seg|ter|qua|qui|sex)\s-\s(\d{2}:\d{2})\s-\s\d{2}:\d{2}$/);
+    if (match) {
+      return {
+        dia: match[1],
+        horarioInicio: match[2]
+      };
+    }
+    return null;
+  };
+
+  const dados1 = extrairDados(str1);
+  const dados2 = extrairDados(str2);
+
+  if (!dados1 || !dados2) {
+    return 0;
+  }
+  if (dados1.dia !== dados2.dia) {
+    const dias = ['seg', 'ter', 'qua', 'qui', 'sex'];
+    return dias.indexOf(dados1.dia) - dias.indexOf(dados2.dia);
+  } else {
+    return dados1.horarioInicio.localeCompare(dados2.horarioInicio);
+  }
+}
+
+function load_students(grade) {
+  fetch("http://localhost:3000/api/students")
+    .then((response) => response.json())
+    .then((data) => {
+      const days = [
+        "monday-list",
+        "tuesday-list",
+        "wednesday-list",
+        "thursday-list",
+        "friday-list",
+      ];
+
+      data.forEach((student) => {
+        Student_reference = new Date(student.date[0]);
+        const studentGradeId = student.gradeReference[0];
+        const studentGradeDescription = grade.find(
+          (gradeItem) => gradeItem._id === studentGradeId
+        ).description;
+
+        let cont = 0;
+
+        const week = new Date().getTime() + 7 * 24 * 60 * 60 * 1000; //somando uma semana em ms
+
+        days.forEach((day) => {
+          if (
+            Student_reference.getDay() == cont &&
+            Student_reference.getTime() < week
+          ) {
+            const studentItem = document.createElement("div");
+            studentItem.innerHTML = `<div class=\"student-print\"><div class="removeButton" style="display: none;"onclick="removeStudent('${student._id}')">X</div><strong>Nome:</strong> ${student.name}<br> <strong>Horário:</strong> ${studentGradeDescription}</div>`;
+
+            document.getElementById(day).appendChild(studentItem);
+          }
+          cont++;
+        });
+      });
+    })
+    .catch((error) => {
+      console.error("Erro ao obter os estudantes disponíveis:", error);
+    });
+}
+
+function updateSelect() {
+  let select = document.getElementById("gradeReference");
+
+  select.disabled = false;
+  select.value = "-1";
+
+  const inputDate = new Date(document.getElementById("date").value);
+
+  const filterDictionary = {
+    0: "seg",
+    1: "ter",
+    2: "qua",
+    3: "qui",
+    4: "sex",
+  };
+
+  let filter = filterDictionary[inputDate.getDay()];
+
+  Array.from(select.options).forEach((option) => {
+    const display = option.text.includes(filter);
+    option.style.display = display ? "block" : "none";
+  });
+}
+
+fetch("http://localhost:3000/api/grades")
+  .then((response) => response.json())
+  .then((data) => {
+    const gradesListDiv = document.getElementById('gradeList');
+    const gradeDropdown = document.getElementById("gradeReference");
+    const newMonitorGrade = document.getElementById("newMonitorGrade");
+
+    gradesListDiv.innerHTML = "";
+
+    data = data.sort((a,b) => a._id - b._id)
+
+    document.getElementById("nextGrade").value = String(Number(data[data.length - 1]._id) + 1);
+
+    data = data.sort((a,b) => sortGrade(a.description,b.description))
+
+    data.forEach((grade) => {
+      const gradeItem = document.createElement("div");
+      gradeItem.innerHTML = `<div class="removeButton" style="display: none;"onclick="removeGrade(${grade._id})">X</div>${grade.description} (${grade._id})`;
+      gradeItem.classList.add("gradeInstance")
+      gradesListDiv.appendChild(gradeItem);
+
+      const option = document.createElement("option");
+      option.value = grade._id;
+      option.text = `${grade.description} (${grade._id})`;
+      gradeDropdown.appendChild(option);
+
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.classList.add("checkbox");
+      checkbox.value = grade._id;
+
+      const label = document.createElement("div");
+      label.innerText = grade.description;
+
+      const monitorGrade = document.createElement("div");
+      monitorGrade.appendChild(label);
+      monitorGrade.appendChild(checkbox);
+      monitorGrade.classList.add("monitorFormOption");
+
+      newMonitorGrade.appendChild(monitorGrade);
+    });
+    load_students(data);
+    load_monitors(data)
+  })
+  .catch((error) => {
+    console.error("Erro ao obter as grade disponíveis:", error);
+  });
